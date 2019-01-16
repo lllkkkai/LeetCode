7a236e9f9e725d48c816872675f8fdf00da107d9
MrLazyEL <1594143235@qq.com>
2019-01-16 17:05:31 +0800

    1.16.19

---
+++ [O]Intersection of Two Linked Lists/Intersection_of_Two_Linked_Lists.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    struct ListNode *pa=headA,*pb=headB;
+    int lengthA=0,lengthB=0;
+    while(pa) {pa=pa->next;lengthA++;}
+    while(pb) {pb=pb->next;lengthB++;}
+    if(lengthA<=lengthB){
+        int n=lengthB-lengthA;
+        pa=headA;pb=headB;
+        while(n) {pb=pb->next;n--;}
+    }
+    else{
+        int n=lengthA-lengthB;
+        pa=headA;pb=headB;
+        while(n) {pa=pa->next;n--;}
+    }
+    while(pa!=pb){
+       pa=pa->next;
+       pb=pb->next;
+    }
+    return pa;
+}
