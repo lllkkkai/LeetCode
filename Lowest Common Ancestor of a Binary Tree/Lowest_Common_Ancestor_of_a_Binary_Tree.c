5d7c171c18691d7f078bd39e58d7b4499e58dcb1
MrLazyEL <1594143235@qq.com>
2019-01-21 11:34:39 +0800

    1.21.19

---
+++ Lowest Common Ancestor of a Binary Tree/Lowest_Common_Ancestor_of_a_Binary_Tree.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if(!root || p == root || q == root)
+        return root;
+    struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    struct TreeNode *right = lowestCommonAncestor(root->right, p , q);
+    if(left && right) 
+        return root;
+    return left ? left : right;
+}
